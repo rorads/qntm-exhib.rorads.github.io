57b27c13414ca514326efc0e19b2917fee9338f8
Rory Scott <rory09@gmail.com>
2025-03-20 16:51:34 +0000

    figured out audio playback issue - copying files to assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
@@ -28,6 +29,11 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "src/assets", to: "assets" }
+      ],
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
